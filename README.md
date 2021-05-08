80a314e4de4ed10f795703e03459514868650ac4
Arden Rasmussen <ardenisthebest@gmail.com>
2021-05-07 22:23:07 -0700

    implemented using webpack

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Personal Wiki static site generator
 [![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
 [![Version](https://img.shields.io/npm/v/loci.svg)](https://npmjs.org/package/loci)
 [![Downloads/week](https://img.shields.io/npm/dw/loci.svg)](https://npmjs.org/package/loci)
-[![License](https://img.shields.io/npm/l/loci.svg)](https://github.com/Nedra1998/loci/blob/master/package.json)
+[![License](https://img.shields.io/npm/l/loci.svg)](https://github.com/Nedra1998/Loci/blob/master/package.json)
 
 <!-- toc -->
 * [Usage](#usage)
@@ -19,7 +19,7 @@ $ npm install -g loci
 $ loci COMMAND
 running command...
 $ loci (-v|--version|version)
-loci/0.2.0 linux-x64 node-v16.0.0
+loci/0.1.0 linux-x64 node-v16.0.0
 $ loci --help [COMMAND]
 USAGE
   $ loci COMMAND
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,126 @@
-import { join } from "path";
 import { Command, flags } from "@oclif/command";
-import { loadConfig, Config } from "./config";
-import { collectSourceFiles, configureLoaders, preProcess } from "./workspace";
+import * as path from "path";
+import logSymbols from "log-symbols";
+
+import { webpack, Configuration } from "webpack";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import * as MinicssExtractPlugin from "mini-css-extract-plugin";
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 class Loci extends Command {
   static description = "describe the command here";
 
   static flags = {
-    version: flags.version({ char: "V" }),
+    version: flags.version({ char: "v" }),
     help: flags.help({ char: "h" }),
-    source: flags.string({
-      char: "s",
-      default: process.cwd(),
-      description: "wiki root source directory",
-    }),
-    dest: flags.string({
+    directory: flags.string({
       char: "d",
-      description: "wiki site output directory",
+      default: process.cwd(),
+      description: "directory of wiki repository",
     }),
-    config: flags.string({
-      description: "wiki configuration path",
+    output: flags.string({
+      char: "o",
+      default: path.join(process.cwd(), "dist"),
+      description: "output directory for generated files",
     }),
-    templates: flags.string({
-      description: "HTML templates directory",
+    dev: flags.boolean({ description: "set webpack to development mode" }),
+    prod: flags.boolean({ description: "set webpack to production mode" }),
+    watch: flags.boolean({
+      description: "watch for changes in the source directory",
     }),
+    serve: flags.boolean({ description: "serve webpack server" }),
   };
 
-  static args = [];
+  static args = [{ name: "file" }];
 
   async run() {
     const { args, flags } = this.parse(Loci);
-    const config: Config | null = await loadConfig(
-      flags.config || join(flags.source, "loci.toml")
-    );
-    pretty.display(undefined, false);
-    if (config === null) return;
 
-    const loaders = await configureLoaders(flags.source, config);
-    pretty.display("Configured loaders");
-    const sources = await collectSourceFiles(flags.source, loaders, config);
-    pretty.display("Collected source files");
-    const metadata = await preProcess(sources, loaders, config);
-    pretty.display("PreProcessed files");
-    console.log(metadata);
+    const prod = flags.prod || (!flags.dev && !flags.watch);
+
+    const excludes = [
+      flags.output,
+      path.resolve(path.join(flags.directory, "./.git")),
+    ];
+
+    const markdownRule = {
+      test: /\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|rmd)$/i,
+      exclude: excludes,
+      use: [
+        "html-loader",
+        {
+          loader: "markdown-loader",
+          options: {
+            use: [
+              require("markdown-it-emoji"),
+              require("markdown-it-deflist"),
+              require("markdown-it-sub"),
+              require("markdown-it-mark"),
+              require("markdown-it-sup"),
+              require("markdown-it-footnote"),
+              [require("markdown-it-anchor"), { permalink: true }],
+              require("markdown-it-katex"),
+            ],
+          },
+        },
+      ],
+    };
+    const pdfRule = {
+      test: /\.(pdf)$/i,
+      exclude: excludes,
+      use: ["file-loader"],
+    };
+    const fontRule = {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      exclude: excludes,
+      use: ["file-loader"],
+    };
+    const imageRule = {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      exclude: excludes,
+      use: ["file-loader"],
+    };
+    const videoRule = {
+      test: /\.(mp4)$/i,
+      exclude: excludes,
+      use: ["file-loader"],
+    };
+
+    const config: Configuration = {
+      mode: prod ? "production" : "development",
+      entry: path.resolve(path.join(__dirname, "../resources/index.js")),
+      output: {
+        filename: "bundle.js",
+        chunkFilename: "[chunkhash].js",
+        path: flags.output,
+      },
+      devtool: prod ? undefined : "inline-source-map",
+      optimization: {
+        minimize: prod,
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          title: "Loci",
+          template: path.resolve(
+            path.join(__dirname, "../resources/index.ejs")
+          ),
+        }),
+        new MinicssExtractPlugin(),
+        new CleanWebpackPlugin(),
+        new FaviconsWebpackPlugin(
+          path.join(__dirname, "../resources/favicon.svg")
+        ),
+      ],
+      module: {
+        rules: [markdownRule, videoRule, imageRule, pdfRule, fontRule],
+      },
+    };
+    const compiler = webpack(config);
+    compiler.run((err, stats) => {
+      if (err || stats === undefined) return console.error(err);
+      console.log(stats.toString({ colors: true }));
+    });
   }
 }
 
